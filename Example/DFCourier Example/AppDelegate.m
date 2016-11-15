845aca688f89801143cb894be30e1617f03cbeec
fangqiuming <fangqiuming@outlook.com>
2016-11-15 18:31:36 +0800

    Recreate example project.

--- Example/DFCourier Example/AppDelegate.m
+++ Example/DFCourier Example/AppDelegate.m
@@ -1,8 +1,8 @@
 //
 //  AppDelegate.m
-//  DFCourierExample
+//  DFCourier Example
 //
-//  Created by 方秋鸣 on 2016/11/14.
+//  Created by 方秋鸣 on 2016/11/15.
 //  Copyright © 2016年 方秋鸣. All rights reserved.
 //
 
--- Example/DFCourier Example/main.m
+++ Example/DFCourier Example/main.m
@@ -1,8 +1,8 @@
 //
 //  main.m
-//  DFCourierExample
+//  DFCourier Example
 //
-//  Created by 方秋鸣 on 2016/11/14.
+//  Created by 方秋鸣 on 2016/11/15.
 //  Copyright © 2016年 方秋鸣. All rights reserved.
 //
 
